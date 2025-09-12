5e76de22473e2840e0398ee97ee6b5f165501a42
Mauricio Rodriguez <maurijrp2001@gmail.com>
2025-09-11 22:48:40 -0600

    comment auto open tree in nvim

--- stow/nvim/.config/nvim/lua/chadrc.lua
+++ stow/nvim/.config/nvim/lua/chadrc.lua
@@ -1,24 +1,35 @@
--- This file needs to have same structure as nvconfig.lua 
--- https://github.com/NvChad/ui/blob/v3.0/lua/nvconfig.lua
--- Please read that file to know all available options :( 
-
 ---@type ChadrcConfig
 local M = {}
 
-M.base46 = {
-	theme = "onedark",
+M.ui = { theme = "catppuccin" }
 
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
-}
+-- Run after plugins are ready
+vim.api.nvim_create_autocmd("User", {
+  pattern = "VeryLazy",
+  callback = function()
+    -- (still load any other custom code if you want)
+    pcall(require, "custom.init")
 
--- M.nvdash = { load_on_startup = true }
--- M.ui = {
---       tabufline = {
---          lazyload = false
---      }
--- }
+    -- Open nvim-tree on start (no file) or when starting on a dir
+    -- local ok, api = pcall(require, "nvim-tree.api")
+    -- if not ok then return end
+    --
+    -- local argc = vim.fn.argc()
+    -- if argc == 0 then
+    --   vim.schedule(function() api.tree.open() end)
+    --   return
+    -- end
+    --
+    -- local argv = vim.fn.argv()
+    -- if argc == 1 and vim.fn.isdirectory(argv[1]) == 1 then
+    --   vim.cmd.cd(argv[1])
+    --   vim.schedule(function() api.tree.open() end)
+    -- end
+  end,
+})
+
+-- (optional) M.mappings = require "custom.mappings"
+-- (optional) M.plugins  = "custom.plugins"
 
 return M
+
--- stow/nvim/.config/nvim/lua/custom/init.lua
+++ stow/nvim/.config/nvim/lua/custom/init.lua
@@ -3,18 +3,28 @@
 local autocmd = vim.api.nvim_create_autocmd
 
 -- Auto-open nvim-tree when starting nvim without args or with a directory
-autocmd({ "VimEnter" }, {
-  callback = function(data)
-    -- if a file is passed, don't open tree
-    if vim.fn.argc() > 0 then
-      return
-    end
+-- autocmd({ "VimEnter" }, {
+--   callback = function(data)
+--     -- if a file is passed, don't open tree
+--     if vim.fn.argc() > 0 then
+--       return
+--     end
+--
+--     -- open the tree
+--     require("nvim-tree.api").tree.open()
+--   end,
+-- })
 
-    -- open the tree
-    require("nvim-tree.api").tree.open()
+-- if vim.fn.isdirectory(data.file) == 1 then
+--   require("nvim-tree.api").tree.open()
+-- end
+
+vim.api.nvim_create_autocmd("QuitPre", {
+  callback = function()
+    local tree_open = require("nvim-tree.api").tree.is_visible()
+    if tree_open and #vim.api.nvim_list_wins() == 1 then
+      vim.cmd("qall")
+    end
   end,
 })
 
-if vim.fn.isdirectory(data.file) == 1 then
-  require("nvim-tree.api").tree.open()
-end
